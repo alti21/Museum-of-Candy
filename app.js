d69249d5224756b601d4c2e6a63fc00d31acffea
Altemush Bhatti <altemush@gmail.com>
2019-10-24 15:43:41 -0700

    Added tickets page with dynamic date, and added navbar link functionality
    Linked home page to Home navbar link and ticksts page to Tickets navbar link. Added tickets page with image, information, and dynamic date showing what day visitors can visit the museum.

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ $(function(){
     })
 });
 
-document.querySelector(".nav-link").addEventListener("click",function()
-{
-    alert("clicked!");
-});
+// document.querySelector(".nav-link").addEventListener("click",function()
+// {
+//     alert("clicked!");
+// });
+
+
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];   
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];    
+var tomorrow = new Date();
+tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
+document.getElementById("spanDate").innerHTML = days[tomorrow.getDay()] + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getDate()+ ", " + tomorrow.getFullYear();
