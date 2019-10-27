ebe68ad6d819fcf80747e3682adcf55f8bed60af
Altemush Bhatti <altemush@gmail.com>
2019-10-26 20:11:18 -0700

    Removed duplicate id's. Fix bug where users could only increase the amount of adults tickets
    Now users are able to increase the amount of any type of ticket. Also removed duplacate id's

--- app.js
+++ app.js
@@ -17,22 +17,118 @@ var tomorrow = new Date();
 tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
 document.getElementById("spanDate").innerHTML = days[tomorrow.getDay()] + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getDate() + ", " + tomorrow.getFullYear();
 
-var ticketDisplay = document.querySelector("#no_of_tickets");
-var tickets = 0;
-var subtractTickets = document.querySelector("#subtract");
-var addTickets = document.querySelector("#add");
-subtractTickets.addEventListener("click",function(){
-    if(tickets === 0)
+var ticketDisplay = document.querySelectorAll(".no_of_tickets");
+var tickets = [0,0,0,0,0];
+var subtractTickets = document.querySelectorAll(".subtract");
+var addTickets = document.querySelectorAll(".add");
+
+
+
+subtractTickets[0].addEventListener("click",function(){
+    if(tickets[0] === 0)
     {
         alert("Cannot have negative amount of tickets");
     }
-    else if(tickets > 0)
-    {
-        tickets--;
-        ticketDisplay.innerHTML = tickets;
+    else if(tickets[0] > 0)
+    { 
+        tickets[0]--;
+        ticketDisplay[0].innerHTML = tickets[0];
     }
 });
-addTickets.addEventListener("click",function(){
-    tickets++;
-    ticketDisplay.innerHTML = tickets; 
+addTickets[0].addEventListener("click",function(){
+        tickets[0]++;
+        
+        ticketDisplay[0].innerHTML = tickets[0]; 
 });
+
+subtractTickets[1].addEventListener("click",function(){
+    if(tickets[1] === 0)
+    {
+        alert("Cannot have negative amount of tickets");
+    }
+    else if(tickets[1] > 0)
+    { 
+        tickets[1]--;
+        ticketDisplay[1].innerHTML = tickets[1];
+    }
+});
+addTickets[1].addEventListener("click",function(){
+        tickets[1]++;
+        
+        ticketDisplay[1].innerHTML = tickets[1]; 
+});
+
+subtractTickets[4].addEventListener("click",function(){
+    if(tickets[4] === 0)
+    {
+        alert("Cannot have negative amount of tickets");
+    }
+    else if(tickets[4] > 0)
+    { 
+        tickets[4]--;
+        ticketDisplay[4].innerHTML = tickets[4];
+    }
+});
+addTickets[4].addEventListener("click",function(){
+        tickets[4]++;
+        
+        ticketDisplay[4].innerHTML = tickets[4]; 
+});
+
+subtractTickets[2].addEventListener("click",function(){
+    if(tickets[2] === 0)
+    {
+        alert("Cannot have negative amount of tickets");
+    }
+    else if(tickets[2] > 0)
+    { 
+        tickets[2]--;
+        ticketDisplay[2].innerHTML = tickets[2];
+    }
+});
+addTickets[2].addEventListener("click",function(){
+        tickets[2]++;
+        
+        ticketDisplay[2].innerHTML = tickets[2]; 
+});
+
+subtractTickets[3].addEventListener("click",function(){
+    if(tickets[3] === 0)
+    {
+        alert("Cannot have negative amount of tickets");
+    }
+    else if(tickets[3] > 0)
+    { 
+        tickets[3]--;
+        ticketDisplay[3].innerHTML = tickets[3];
+    }
+});
+addTickets[3].addEventListener("click",function(){
+        tickets[3]++;
+        
+        ticketDisplay[3].innerHTML = tickets[3]; 
+});
+
+
+// var i = 0;
+// while(i < ticketDisplay.length)
+// {
+//     subtractTickets[i].addEventListener("click",function(){
+//         console.log(i);
+//         if(tickets[i] === 0)
+//         {
+//             alert("Cannot have negative amount of tickets");
+//         }
+//         else if(tickets[i] > 0)
+//         { 
+//             tickets[i]--;
+//           //  ticketDisplay[i].innerHTML = tickets[i];
+//         }
+//     });
+//     addTickets[i].addEventListener("click",function(){
+//             tickets[i]++;
+            
+//           //  ticketDisplay[i].innerHTML = tickets[i]; 
+//     });
+// }
+
