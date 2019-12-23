528a0ad16588ff2914b491796504d8cdb50b9a53
Altemush Bhatti <altemush@gmail.com>
2019-12-23 11:26:05 -0800

    Added ticket total price and Continue to Payment button

--- app.js
+++ app.js
@@ -15,15 +15,30 @@ const months = ['January','February','March','April','May','June','July','August
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];    
 let tomorrow = new Date();
 tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
-document.getElementById("spanDate").innerHTML = days[tomorrow.getDay()] + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getDate() + ", " + tomorrow.getFullYear();
-console.log(tomorrow.getDate());
+//document.getElementById("spanDate").innerHTML = days[tomorrow.getDay()] + ", " + months[tomorrow.getMonth()] + " " + tomorrow.getDate() + ", " + tomorrow.getFullYear();
+
+
+
+//Selecting tickets
 let tickets = 0;
+let init = 0;
+let total = 0;
+
 for(let i=0;i<document.querySelectorAll(".no_of_tickets").length;i++)
 {
+    
     document.querySelectorAll(".subtract")[i].addEventListener("click",function(){
         tickets = parseInt(event.target.parentNode.nextElementSibling.innerHTML);
         //event.target returns the button because the click event
         //originally occured on the button (clicked on button)
+        // if(!save)
+        // {
+        //     total = parseInt(event.target.parentNode.parentNode.previousElementSibling.innerHTML);
+        //     save = true;
+        // }
+        
+        //use event bubbling
+        init = parseInt(event.target.parentNode.parentNode.previousElementSibling.innerHTML);
         if(tickets === 0)
         {
             alert("Cannot have negative amount of tickets");
@@ -32,20 +47,32 @@ for(let i=0;i<document.querySelectorAll(".no_of_tickets").length;i++)
         { 
             tickets--;
             event.target.parentNode.nextElementSibling.innerHTML = tickets;
+            total-=init;
         }
+        
+        console.log(total);
     });
     document.querySelectorAll(".add")[i].addEventListener("click",function(){
-            tickets = parseInt(event.target.parentNode.previousElementSibling.innerHTML);
-            tickets++;
-            event.target.parentNode.previousElementSibling.innerHTML = tickets;
+        
+        init = parseInt(event.target.parentNode.parentNode.previousElementSibling.innerHTML);
+            
+        
+        tickets = parseInt(event.target.parentNode.previousElementSibling.innerHTML);
+        tickets++;
+        event.target.parentNode.previousElementSibling.innerHTML = tickets;
+        total+=init;
+        console.log(total);
+        document.querySelector('.total').innerHTML = `Total price: ${total}`;
     });
 }
+// Total
+let totalDisplay = document.createElement('h1').appendChild( document.createTextNode(total) );
 
+
+
+//forming the calendar
 let all = document.querySelectorAll(".days > * > * ");
 
-//Array.from(all).forEach(cur => cur.innerHTML = 5);
-
-console.log(tomorrow.getDay());
 let year = tomorrow.getFullYear();
 let currentMonth = tomorrow.getMonth();
 let today= new Date(months[currentMonth] +  " 1, "+ year);
@@ -53,17 +80,31 @@ let start_day = today.getDay() + 1;   // starts with 0
 
 console.log(`start day is ${start_day}`);//start_day is 4, January 1st of 2020 is on 4th day of the week
 
+let selected = false;
 const fillDays = () => {
     for(let i=start_day; i <= 31; i++)
     {//display days of the month
         document.querySelectorAll('td')[i-1].innerHTML = i-(start_day - 1);//7
         //document.querySelectorAll(".days > * > * ")[i].innerHTML = i;
+        
+        
     }
+    selected = true;
 };
-
 fillDays();
 
+let er;
+document.querySelector('.pay').addEventListener('click',() => {
+    if(!selected)
+    {
+        document.querySelector('.pay').appendChild( document.createTextNode(`Please select days`) );
+    }
+});
 
+
+
+
+//Selecting dates of visit
 Array.from(document.querySelectorAll('td')).forEach(el => {
     //console.log(el.textContent);
     if(el.textContent !== '')
@@ -77,8 +118,4 @@ Array.from(document.querySelectorAll('td')).forEach(el => {
 });
 
 
-// for(const cur of months)
-// {
-//     fillDays(cur);
-// }
-//use array of size 365, loop through each month, for each month, loop to correct spot in array of size 365
+
